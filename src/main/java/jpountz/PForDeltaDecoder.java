11b1910052f74d9f0adf819049bcce153a36feec
Greg Miller <gmiller@amazon.com>
2021-04-07 18:25:29 -0700

    cp

--- src/main/java/jpountz/PForDeltaDecoder.java
+++ src/main/java/jpountz/PForDeltaDecoder.java
@@ -9,10 +9,12 @@ final class PForDeltaDecoder {
 
     // IDENTITY_PLUS_ONE[i] == i+1
     private static final long[] IDENTITY_PLUS_ONE = new long[ForUtil.BLOCK_SIZE];
+    private static final long[] IDENTITY_PLUS_TWO = new long[ForUtil.BLOCK_SIZE];
 
     static {
         for (int i = 0; i < ForUtil.BLOCK_SIZE; ++i) {
             IDENTITY_PLUS_ONE[i] = i + 1;
+            IDENTITY_PLUS_TWO[i] = (i + 1) * 2;
         }
     }
 
@@ -25,8 +27,9 @@ final class PForDeltaDecoder {
     }
 
     private static void prefixSumOf(long val, long[] arr, long base) {
+        System.arraycopy(IDENTITY_PLUS_TWO, 0, arr, 0, ForUtil.BLOCK_SIZE);
         for (int i = 0; i < ForUtil.BLOCK_SIZE; i++) {
-            arr[i] = IDENTITY_PLUS_ONE[i] * val + base;
+            arr[i] += base;
         }
     }
 
